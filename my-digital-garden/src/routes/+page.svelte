e09eb2a28f74488c6fc875806525a9f2fb562059
Alisa <alice.afanasieva@gmail.com>
2024-11-08 16:39:42 +0100

    Bestanden opgezet #57

--- my-digital-garden/src/routes/+page.svelte
+++ my-digital-garden/src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+  
+  <main>
+    <header>
+      <div class="menu">☰</div>
+      <h1>digital_garden</h1>
+    </header>
+  
+    <section class="intro">
+      <p class="ascii-art">
+        · . · '' ¯ ` · . · . · * · . · `` ¯ ` · . · . · * · ·
+      </p>
+      <h2>Welcome to my digital garden ^_^</h2>
+      <p>This is my blog where I publish my projects, lessons, and more.</p>
+    </section>
+  
+    <section class="projects">
+      <h3>Projects</h3>
+      <div class="grid">
+
+      </div>
+      <a href="/projects" class="see-more">see more</a>
+    </section>
+  
+    <section class="learning-journal">
+      <h3>Learning Journal</h3>
+      <div class="grid">
+
+      </div>
+      <a href="/learning-journal" class="see-more">see more</a>
+    </section>
+  
+    <section class="web-love">
+      <h3>We ❤️ Web</h3>
+      <div class="grid">
+
+      </div>
+      <a href="/we-love-web" class="see-more">see more</a>
+    </section>
+  
+  </main>
+  
+  <style>
+
+  </style>
