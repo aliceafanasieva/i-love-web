720e13d2e65ab936a01577c053c7ad9d7944cbe3
Alisa <alice.afanasieva@gmail.com>
2024-12-11 11:37:09 +0100

    hamburger/navigatie/ascii-animatie
    1. Ervoor gezorgd dat de hamburger menu alleen te zien is bij @media width < 50em en bij > 50em de navigatie balk verschijnt.
    
    2. kaomoji animatie gemaakt bij welkomsbericht op de homepage met @keyframes.

--- my-digital-garden/src/routes/+page.svelte
+++ my-digital-garden/src/routes/+page.svelte
@@ -4,15 +4,26 @@
 </script>
 
 <header>
-    <Hamburger 
-        type="squeeze" 
-        --color="var(--main-text-color)" 
-        --border-radius="0.063rem"
-        --layer-height="0.125rem"  
-        --layer-spacing="0.438rem"
-    />
+    <div class="hamburger-menu">
+        <Hamburger 
+            type="squeeze" 
+            --color="var(--main-text-color)" 
+            --border-radius="0.063rem"
+            --layer-height="0.125rem"  
+            --layer-spacing="0.438rem"
+        />
+    </div>
 
     <h1 class="logo">digital_garden</h1>
+
+    <nav class="nav-list">
+        <ul>
+            <li><a href="#about-me">About Me</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#learning-journal">Learning Journal</a></li>
+            <li><a href="#we-love-web">We love Web</a></li>
+        </ul>
+    </nav>
 </header>
 
 <main>
@@ -21,7 +32,8 @@
             <p class="ascii-art">
                 ˚₊ ˚ ‧₊ .:･˚ *₊ ˚ ‧₊ .:･˚₊ 
             </p>
-            <h2>Welcome to my digital garden (^_^*) </h2>
+            <!-- svelte-ignore a11y_missing_content -->
+            <h1 class="kao_animate"> </h1>
             <p class="ascii-art">
                 ˚₊ ˚ ‧₊ .:･˚ *₊ ˚ ‧₊ .:･˚₊ 
             </p>
@@ -30,7 +42,7 @@
     </section>
 
     <section class="category projects">
-        <h3 class="title_category">Projects</h3>
+        <h2 class="title_category">Projects</h2>
         <div class="grid">
             <ProjectCard title="naam van project" description="beschrijving van project" image="./flowers/lotus.png" />
             <ProjectCard title="naam van project" description="beschrijving van project" image="./flowers/orchid-dots-2.png" />
@@ -43,7 +55,7 @@
     </section>
 
     <section class="category learning-journal">
-        <h3 class="title_category">Learning Journal</h3>
+        <h2 class="title_category">Learning Journal</h2>
         <div class="grid">
             <!-- Learning journal -->
             <ProjectCard title="naam van project" description="beschrijving van project" image="./flowers/lotus.png" />
@@ -57,7 +69,7 @@
     </section>
 
     <section class="category web-love">
-        <h3 class="title_category">We love Web</h3>
+        <h2 class="title_category">We love Web</h2>
         <div class="grid">
             <!-- We love web -->
             <ProjectCard title="naam van project" description="beschrijving van project" image="./flowers/lotus.png" />
