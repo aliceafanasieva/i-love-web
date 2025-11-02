5281bd48273fde493a4a67a4df73ee63d97642eb
Alisa <alice.afanasieva@gmail.com>
2025-11-02 18:05:51 +0100

    feat: netlify devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@ export default defineNuxtConfig({
   modules: ['@nuxt/content'],
   css: ['@/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+    nitro: {
+    preset: 'netlify'
+  }
 })
